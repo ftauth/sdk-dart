42356b9f851ddfec621ea05beb280bed7fb95dd4
Dillon Nys <dnys1@asu.edu>
2021-01-02 17:51:51 -0700

    Add Flutter storage provider

--- ftauth_flutter/lib/src/ftauth_extensions.dart
+++ ftauth_flutter/lib/src/ftauth_extensions.dart
@@ -1,9 +1,28 @@
-import 'package:ftauth/ftauth.dart';
+import 'dart:convert';
+import 'dart:typed_data';
 
-import 'authorizer.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:ftauth/ftauth.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+import 'flutter_authorizer.dart';
 
 extension FTAuthX on FTAuth {
-  void initFlutter() {
+  Future<void> initFlutter() async {
+    await Hive.initFlutter();
+    final secureStorage = const FlutterSecureStorage();
+    var containsEncryptionKey = await secureStorage.containsKey(key: 'key');
+    Uint8List encryptionKey;
+    if (containsEncryptionKey) {
+      final encodedKey = await secureStorage.read(key: 'key');
+      encryptionKey = base64Decode(encodedKey);
+    } else {
+      encryptionKey = Hive.generateSecureKey();
+      await secureStorage.write(
+          key: 'key', value: base64UrlEncode(encryptionKey));
+    }
     authorizer = FlutterAuthorizer();
+    await storageRepo.init(encryptionKey: encryptionKey);
   }
 }
