9620c0672da38992fb7811a1743dc6660c366d78
Dillon Nys <dillon.andre.nys@gmail.com>
2021-12-07 19:14:16 -0700

    Update mono_repo & add Flutter pkgs

--- tool/ci.sh
+++ tool/ci.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Created with package:mono_repo v5.0.4
+# Created with package:mono_repo v6.0.0
 
 # Support built in commands on windows out of the box.
 # When it is a flutter repo (check the pubspec.yaml for "sdk: flutter")
@@ -86,6 +86,10 @@ for PKG in ${PKGS}; do
       test_1)
         echo 'dart test -p chrome --coverage=coverage'
         dart test -p chrome --coverage=coverage || EXIT_CODE=$?
+        ;;
+      test_2)
+        echo 'flutter test'
+        flutter test || EXIT_CODE=$?
         ;;
       *)
         echo -e "\033[31mUnknown TASK '${TASK}' - TERMINATING JOB\033[0m"
